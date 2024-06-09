e29c272f891a85c8c2f6e2540be53802c7f501c2
Harsh Pandey <47934068+harshpandey29@users.noreply.github.com>
2024-06-09 18:31:55 +0530

    uploaded new files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser")
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/Sample",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect("mongodb+srv://admin123:admin123@cluster0.ryswnrs.mongodb.net/",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log("db connected")
     
 }).catch((err)=>{
@@ -23,6 +23,7 @@ const productSchema = new mongoose.Schema({
 
 const product = new mongoose.model("product",productSchema)
 
+//create-postrequest
 app.post("/api/v1/product/new",async(req,res)=>{
     const Product = await product.create(req.body);
     res.status(200).json({
@@ -31,7 +32,15 @@ app.post("/api/v1/product/new",async(req,res)=>{
     })
 
 })
+//Read-getrequest
 
+app.get("/api/v1/products",async(req,res)=>{
+    const products = await product.find();
+    res.status(200).json({
+        sucess:true,
+        products
+    })
+})
 
 app.listen(4500,()=>{
     console.log("vicky is a good boy  http://localhost:4500");
